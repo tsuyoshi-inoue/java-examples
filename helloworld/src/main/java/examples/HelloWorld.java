53ee51a257540924a8bba4ff90a552c4ffc2dfc7
剛志 <@inoue-PC>
2016-02-21 13:52:49 +0900

    Implements hello world display.

---
+++ helloworld/src/main/java/examples/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package examples;
+
+/**
+ * @author Tsuyoshi Inoue
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
